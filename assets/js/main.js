7a8f14d60e75c50f8ef1bb4aa3d206ecf4cbb678
Paula <paula.poncez@usach.cl>
2017-05-09 12:52:28 -0300

    agregar función addStudent() en main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-alert('hola mundo');
+function addStudent(nombreCampo){
+	var campo = "";
+	do{
+		campo = prompt("Ingrese " + nombreCampo);
+	}while(campo == "")
+	return campo;
+}
+
+var campos = ["Nombre Estudiante", "Porcentaje Técnico", "Porcentaje Habilidades Socio-Emocionales"];
+var respuestas = [];
+
+for (var i = 0; i < campos.length ; i++) {
+	respuestas[i] = addStudent(campos[i])
+}
+
+campos.forEach(function(el){
+	respuestas.push(addStudent(el));
+})
+//id impresion-student
