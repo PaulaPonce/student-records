c09408e926f9eefead45a5be5695a326ef3b54c2
marcecabelloherrera <marcecabello.h@gmail.com>
2017-05-09 12:47:35 -0300

    variables dentro funcion addStudent

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,3 @@
-alert('hola mundo');
+var elNombre = addStudent('Ingrese su Nombre y Apellido');
+var losPuntos = addStudent('Ingrese su Porcentaje Técnico');
+var laVida = addStudent('Ingrese sus Habilidades Socio-Emocionales');
