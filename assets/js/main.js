f96579484c99a8a2860ee3070d7519a1f14b2890
amalakamala <amalakamala.a@gmail.com>
2017-05-09 13:27:01 -0300

    constructor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-alert('hola mundo');
+var arreglo = [];
+// El arreglo queda arriba o en una variable?
+
+function Student(nombre,points,life){
+	this.id = id;
+	this.nombre = nombre;
+	this.points = points;
+	this.life = life;
+	this.status = 'Active';
+}
+
+
+
+
+
+
