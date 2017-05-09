35eb2634e8f9e7d547e902867b28349f477a9a3a
marcecabelloherrera <marcecabello.h@gmail.com>
2017-05-09 13:04:44 -0300

    funcion para pedir prompt con sus respectivas variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+
+function addStudent(nombreCampo){
+	var campo = "";
+	do{
+		campo = prompt("Ingrese " + nombreCampo);
+	}while(campo==="")
+	return campo;
+}
+
 var elNombre = addStudent('Ingrese su Nombre y Apellido');
 var losPuntos = addStudent('Ingrese su Porcentaje Técnico');
 var laVida = addStudent('Ingrese sus Habilidades Socio-Emocionales');
+
+	
