ff9e49637e9ce6cc32ea8a4fb6446e203d99ad0e
Melissa <melissa.pacheco.martinez@gmail.com>
2017-05-09 12:34:04 -0300

    funcion imprimir

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
-alert('hola mundo');
+function printAll(){
+	arreglo.forEach(function(cv){
+		document.getElementById("impresion-student").innerHTML+=cv.nombre+"<br>"+cv.points+"<br>"+cv.life+"<br>"+cv.status
+	})
+}
